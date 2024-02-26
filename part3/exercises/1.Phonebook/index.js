003f95aa5bfbb298471189a2b5da4a1842d7e189
Miko Velasco Ramirez <launchx08269@innovaccion.mx>
2024-02-26 16:03:03 -0600

    3. feat: returning phonebook's list.

--- part3/exercises/1.Phonebook/index.js
+++ part3/exercises/1.Phonebook/index.js
@@ -3,4 +3,34 @@ const app = express();
 
 const PORT = 3001;
 
+// Source data
+const dataSrc = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+// Endpoints
+
+app.get('/api/persons', (req, res) => {
+    res.json(dataSrc)
+});
+
 app.listen(PORT, () => console.log(`server running at PORT ${PORT}`))
